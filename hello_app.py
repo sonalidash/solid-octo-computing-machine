6756d9fe3a582ece77603dd3053d8f48854c9fa5
sonalidash <sonali_dash@icloud.com>
2016-04-22 17:51:33 -0700

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -17,14 +17,7 @@ def hello():
     # find a way to pick something
     typing_choice = random.choice(typing_options)
     
-    # a way to tell the user what to do
-    print """This is the typing program picker.
-    It will pick a random typing_choice from a list.
-    The list is kept here %s
-    Todays typing choice is...
-
-    %s
-    """ % (url_for_list, typing_choice)
+    
     return typing_choice
 
 if __name__ == "__main__":
